761511a2d6b89eef84a8e342d1b8cabbd1f8f07d
ngochoawindy <ngochoawindy@gmail.com>
2024-11-09 16:55:56 +0100

    generate kmer

--- dna/dna--1.0.sql
+++ dna/dna--1.0.sql
@@ -26,3 +26,106 @@ CREATE FUNCTION length(dna)
   RETURNS int4
   AS 'MODULE_PATHNAME', 'dna_length'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+
+/*------------*/
+/******************************************************************************
+ * Input/Output
+ ******************************************************************************/
+
+CREATE OR REPLACE FUNCTION kmer_in(cstring)
+    RETURNS kmer
+    AS 'MODULE_PATHNAME'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION kmer_out(kmer)
+    RETURNS cstring
+    AS 'MODULE_PATHNAME'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+-- CREATE OR REPLACE FUNCTION kmer_recv(internal)
+--     RETURNS kmer
+--     AS 'MODULE_PATHNAME'
+--     LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+-- CREATE OR REPLACE FUNCTION kmer_send(kmer)
+--     RETURNS bytea
+--     AS 'MODULE_PATHNAME'
+--     LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE TYPE kmer (
+    internallength = 32,
+    input          = kmer_in,
+    output         = kmer_out,
+    -- receive        = kmer_recv,
+    -- send           = kmer_send,
+    alignment      = double -- ? Need to replace?
+);
+
+CREATE OR REPLACE FUNCTION kmer(text)
+    RETURNS kmer
+    AS 'MODULE_PATHNAME', 'kmer_cast_from_text'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION text(kmer)
+    RETURNS text
+    AS 'MODULE_PATHNAME', 'kmer_cast_to_text'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE CAST (text as kmer) WITH FUNCTION kmer(text) AS IMPLICIT;
+CREATE CAST (kmer as text) WITH FUNCTION text(kmer);
+
+/******************************************************************************
+ * CONSTRUCTOR
+ ******************************************************************************/
+
+CREATE FUNCTION kmer(int, text)
+    RETURNS kmer
+    AS 'MODULE_PATHNAME', 'kmer_constructor'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+/******************************************************************************
+ * FUNCTIONS
+ ******************************************************************************/
+CREATE FUNCTION length(kmer)
+    RETURNS int
+    AS 'MODULE_PATHNAME', 'kmer_length'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION equals(kmer, kmer)
+    RETURNS boolean
+    AS 'MODULE_PATHNAME', 'kmer_equals'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION starts_with(kmer, kmer)
+    RETURNS boolean
+    AS 'MODULE_PATHNAME', 'kmer_starts_with'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE FUNCTION kmer_starts_with_swapped(kmer, kmer)
+    RETURNS boolean
+    AS 'SELECT starts_with($2, $1)'
+    LANGUAGE SQL IMMUTABLE STRICT PARALLEL SAFE;
+
+/******************************************************************************
+ * OPERATORS
+ ******************************************************************************/
+CREATE OPERATOR = (
+    LEFTARG = kmer, RIGHTARG = kmer,
+    PROCEDURE = equals,
+    COMMUTATOR = =
+);
+
+CREATE OPERATOR ^@ (
+    LEFTARG = kmer, RIGHTARG = kmer,
+    PROCEDURE = kmer_starts_with_swapped
+    -- Commutator?
+);
+
+
+---------
+
+CREATE FUNCTION generate_kmers(dna, integer)
+  RETURNS SETOF kmer
+  AS 'MODULE_PATHNAME', 'generate_kmers'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
--- dna/dna_test.sql
+++ dna/dna_test.sql
@@ -4,3 +4,7 @@ insert into t Values (2,'ATG');
 insert into t Values (3,'ATGX'); 
 insert into t Values (3,'ATGC');
 select test, length(test) from t;
+
+SELECT s.id, s.sequence, k.kmer
+FROM dna_sequences AS s,
+     generate_kmers(s.sequence, 4) AS k(kmer);
